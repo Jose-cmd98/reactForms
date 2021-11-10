1e1fb11d9cf1a6486a67e2409328b833e1565c12
Jose-cmd98 <jose1234roberto@gmail.com>
2021-11-09 22:18:29 -0300

    Material UI install

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,7 +2,24 @@ import React from 'react';
 
 function FormularioCadastro(){
     return(
-        <form action=""><input type="text" /></form>
+        <form action="">
+            <label htmlFor="">Nome</label>
+            <input type="text" />
+
+            <label htmlFor="">Sobrenome</label>
+            <input type="text" />
+
+            <label htmlFor="">CPF</label>
+            <input type="text" />
+
+            <label htmlFor="">Promoções</label>
+            <input type="checkbox" />
+
+            <label htmlFor="">Novidades</label>
+            <input type="checkbox" />
+            
+            <button type="submit">Cadastrar</button>
+            </form>
     )
 }
 
