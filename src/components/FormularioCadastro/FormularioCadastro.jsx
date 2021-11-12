1434cce6c87bd834ab92eddc7a5d0f64d1888d55
Jose-cmd98 <jose1234roberto@gmail.com>
2021-11-11 21:50:16 -0300

    useState Hooks

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,31 +1,65 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField  from '@material-ui/core/TextField';
-import Switch from '@material-ui/core/Switch';
-import  FormControlLabel  from '@material-ui/core/FormControlLabel';
+import React, {useState} from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Switch from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
+function FormularioCadastro() {
+  
+    const [nome, setNome] = useState("");
+    
 
-function FormularioCadastro(){
-    return(
-        <form action="">
-            <TextField id="nome" label="Nome" variant="outlined" fullWidth margin="normal"/>
-            
+  return (
+    <form onSubmit={(event) =>{
+        event.preventDefault();
+        console.log(nome);
+    }}>
+      <TextField
+      value={nome}
+        onChange={(event) => {
+          setNome(event.target.value);
+          if(nome.length >=3){
+             setNome(nome.substr(0, 3));
+          }
+        }}
+        id="nome"
+        label="Nome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
 
-            <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth margin="normal"/>
+      <TextField
+        id="sobrenome"
+        label="Sobrenome"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
 
-            <TextField id="cpf" label="CPF" variant="outlined" fullWidth margin="normal"/>
+      <TextField
+        id="cpf"
+        label="CPF"
+        variant="outlined"
+        fullWidth
+        margin="normal"
+      />
 
+      <FormControlLabel
+        label="Promoções"
+        control={<Switch name="promocoes" defaultChecked color="primary" />}
+      />
 
-            <FormControlLabel label="Promoções" control={<Switch name="promocoes" defaultChecked color="primary"/>} />
-            
-            <FormControlLabel label="Novidades" control={<Switch name="novidades" defaultChecked color="primary"/>}/>
-            
-            
-            <Button variant="contained" color="primary">
-                Cadastrar
-            </Button>
-            </form>
-    )
+      <FormControlLabel
+        label="Novidades"
+        control={<Switch name="novidades" defaultChecked color="primary" />}
+      />
+
+      <Button type="submit" variant="contained" color="primary">
+        Cadastrar
+      </Button>
+    </form>
+  );
 }
 
 export default FormularioCadastro;
