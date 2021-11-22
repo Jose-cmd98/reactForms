f622411f8c73a48a8d0976eb8a49c0b4d234a5c7
Jose-cmd98 <jose1234roberto@gmail.com>
2021-11-22 20:32:38 -0300

    Trocado SwitchCase por Arrays

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -7,28 +7,33 @@ import DadosEntrega from './DadosEntrega';
 function FormularioCadastro({aoEnviar, validarCPF}) {
   const  [etapaAtual, setEtapaAtual] = useState(0);
 
+  const formularios = [
+    <DadosUsuario aoEnviar={proximo}/>,
+    <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF}/>,
+    <DadosEntrega aoEnviar={aoEnviar} />
+  ]
 
   function proximo(){
     setEtapaAtual(etapaAtual+1);
   }
 
 
-  function formularioAtual(etapa){
-    switch (etapa) {
-      case 0:
-        return <DadosUsuario aoEnviar={proximo}/>;
-      case 1:
-        return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF}/>;
-      case 2:
-        return <DadosEntrega aoEnvair={aoEnviar}/>;
-      default:
-        return <h1>Erro ao Enviar Formulario!!</h1>
-    }
-  }
+  // function formularioAtual(etapa){
+  //   switch (etapa) {
+  //     case 0:
+  //       return <DadosUsuario aoEnviar={proximo}/>;
+  //     case 1:
+  //       return <DadosPessoais aoEnviar={proximo} validarCPF={validarCPF}/>;
+  //     case 2:
+  //       return <DadosEntrega aoEnvair={aoEnviar}/>;
+  //     default:
+  //       return <h1>Erro ao Enviar Formulario!!</h1>
+  //   }
+  // }
 
     return (
       <>
-      {formularioAtual(etapaAtual)}
+      {formularios[etapaAtual]}
       </>
     );
 }
