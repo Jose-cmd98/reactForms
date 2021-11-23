959095c55220fca917be410e78fe17e873483f6d
Jose-cmd98 <jose1234roberto@gmail.com>
2021-11-23 18:36:48 -0300

    UseEffect

--- src/components/FormularioCadastro/DadosEntrega.jsx
+++ src/components/FormularioCadastro/DadosEntrega.jsx
@@ -1,10 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import { TextField, Button } from "@material-ui/core";
 
-function DadosEntrega() {
+function DadosEntrega(aoEnviar) {
+
+    const [cep, setCep ] = useState("");
+    const [endereco, setEndereco] = useState("");
+    const [numero, setNumero] = useState("");
+    const [cidade, setCidade] = useState("");
+    const [estado, setEstado] = useState("");
+
   return (
-    <form>
+    <form onSubmit={(event) => {
+        event.preventDefault();
+        aoEnviar({cep, endereco, numero, cidade, estado})
+    }}>
       <TextField
+      value={cep}
+      onChange={(event) => {
+          setCep(event.target.value);
+      }}
         id="cep"
         label="Cep"
         type="number"
@@ -14,6 +28,10 @@ function DadosEntrega() {
       />
 
       <TextField
+      value={endereco}
+      onChange={(event) =>{
+          setEndereco(event.target.value);
+      }}
         id="endereço"
         label="Endereço"
         type="text"
@@ -23,6 +41,10 @@ function DadosEntrega() {
       />
 
       <TextField
+      value={numero}
+      onChange={(event) => {
+          setNumero(event.target.value);
+      }}
         id="numero"
         label="Número"
         type="number"
@@ -32,6 +54,10 @@ function DadosEntrega() {
       />
 
       <TextField
+      value={cidade}
+      onChange={(event) => {
+          setCidade(event.target.value);
+      }}
         id="cidade"
         label="Cidade"
         type="text"
@@ -41,6 +67,10 @@ function DadosEntrega() {
       />
 
       <TextField
+      value={estado}
+      onChange={(event) => {
+          setEstado(event.target.value);
+      }}
         id="estado"
         label="Estado"
         type="text"
